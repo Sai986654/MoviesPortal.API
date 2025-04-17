7374aa6bf0ca05d8072e744345cb5251788c81d9
Sai986654 <109938786+Sai986654@users.noreply.github.com>
2025-04-17 17:41:40 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,12 +13,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:3000")
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
+    policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
 });
 
 var app = builder.Build();
